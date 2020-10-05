50a249bdb80b90c3f036d156612acb94be661f09
diegomuner <diegomuner@hotmail.com>
2020-10-04 22:48:19 -0300

    -Added TakeScreenShot to the CustomListener.java
    -In the @BeforeTest added a driver object to the test context
    (this can be done directly on @AfterTest method too
    
    - in the @AfterTest we are adding an if statement to track if the driver
    was null or instantiated at the end

--- AutomationProject-OCT2020/src/main/java/automationprojectoct/CustomListener.java
+++ AutomationProject-OCT2020/src/main/java/automationprojectoct/CustomListener.java
@@ -1,18 +1,53 @@
 package automationprojectoct;
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
+import org.apache.commons.io.FileUtils;
 
 
 
 
-public class CustomListener extends TestListenerAdapter{
+public class CustomListener implements ITestListener{
 	   private int m_count = 0;
+	   WebDriver driver=null;
+		String filePath = "C:\\users\\diego\\Desktop\\";
 
-	   
+	/*   
 	   @Override
 	   public void onTestFailure(ITestResult tr) {
 	      log(tr.getName()+ "--Test method failed\n");
-	   }
+	     // File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	    //codigo para copiar el screenshot en una localizacion deseada
+	    //FileUtils.copyFile(scrFile, new File("C:\\Screenshot\\google.jpg"));
+*/	    
+	   @Override
+	   public void onTestFailure(ITestResult tr) { 
+	    	System.out.println("***** Error "+tr.getName()+" test has failed *****");
+	    	String methodName=tr.getName().toString().trim();
+	        ITestContext context = tr.getTestContext();
+	        WebDriver driver = (WebDriver)tr.getTestContext().getAttribute("webDriver");
+	    	takeScreenShot(methodName, driver);
+	    }
+	    
+	    public void takeScreenShot(String methodName, WebDriver driver) {
+	    	 File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	         //The below method will save the screen shot in d drive with test method name 
+	            try {
+					FileUtils.copyFile(scrFile, new File(filePath+ methodName +".png"));
+					System.out.println("***Placed screen shot in "+filePath+" ***");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	    }      
+	      
+	    
 	   @Override
 	   public void onTestSkipped(ITestResult tr) {
 	      log(tr.getName()+ "--Test method skipped\n");
--- AutomationProject-OCT2020/src/test/java/tests/BaseTest.java
+++ AutomationProject-OCT2020/src/test/java/tests/BaseTest.java
@@ -5,10 +5,17 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+
+@Listeners({automationprojectoct.CustomListener.class}) 
+
+
 
 public class BaseTest {
 	
@@ -16,7 +23,7 @@ public class BaseTest {
 	
 	
 	@BeforeTest
-	public void setUp() {
+	public void setUp(ITestContext context) {
 		//Browser Setup
 		System.out.println("baseTestBeforeTest called");
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\diego\\Downloads\\chromedriver_win32\\chromedriver.exe");
@@ -24,14 +31,27 @@ public class BaseTest {
 		this.driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://www.google.com");
+		context.setAttribute("webDriver", driver);
+		System.out.println("Webdriver created and added to context");
 		
 		
 	}
 	@AfterTest
 	public void tearDown() {
 		//Closing Browser and Driver
-		System.out.println("Closing Browser - tearDown method called");
-		driver.close();
+		 if(driver != null){
+	            driver.quit();
+	            System.out.println("Driver was instantiated. Quitting..");
+	        }else{
+	            System.out.println("Driver was null so nothing to do");
+	        }
+	    }
+	
+	
+	public WebDriver getDriver() {
+		
+		return driver;
+		
 	}
 	
 	
