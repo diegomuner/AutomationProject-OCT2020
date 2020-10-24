d9876ca8c2c436db24dff55797135fd32117819d
diegomuner <diegomuner@hotmail.com>
2020-10-24 14:28:20 -0300

    k

--- AutomationProject-OCT2020/src/test/java/com/automationoct/dataproviders/TestWebPageDataProvider.java
+++ AutomationProject-OCT2020/src/test/java/com/automationoct/dataproviders/TestWebPageDataProvider.java
@@ -3,17 +3,18 @@ package com.automationoct.dataproviders;
 import org.testng.annotations.DataProvider;
 
 public class TestWebPageDataProvider {
+
+	@DataProvider(name = "data1")
+	public Object[][] getUserData() {
+		return new Object[][] { { "Diego1", "password1" }, { "Melanie2", "asdasd1" }, { "Lucky3", "bebedemomi" }, };
+		
+		
+		
+	}
 	
-	
-	@DataProvider(name="data1")
-	public  Object[][] getUserData(){
-        return new Object[][] {
-            {"Diego1", "password1"},
-            {"Melanie2", "medicina1"},
-            {"Lucky3", "bebedemomi"},
-        };
-                }
-	
-	
+	@DataProvider(name = "data2")
+	public Object[][] getUserData2() {
+		return new Object[][] { { "Diego2", "password2" }, { "Melanie3", "asdasd" }, { "Lucky3", "bebeasdi" }, };
+	}
 
 }
